b21f55b09716c30345f8e8e74eb97378c96401f1
Shuyi Sun <35696908+ssun83@users.noreply.github.com>
2024-08-07 12:18:33 -0700

    change to Kotlin format

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,12 @@ dependencyResolutionManagement {
         mavenCentral()
 
         // Jitpack repo needed for Nix Universal SDK cross-dependencies
-        maven { url 'https://jitpack.io' }
+        maven { url = uri("https://jitpack.io") }
 
         // Point to folder-based `nixrepo` repository. If you have stored
         // `nixrepo` in a repository management system, point the url to that
         // location instead
-        maven { url "file://$rootDir/nixrepo" }
+        maven { url = uri("file://$rootDir/nixrepo") }
     }
 }
 
